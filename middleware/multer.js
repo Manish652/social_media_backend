f1b87f7d40a503a54d2dee363328ac9831b80259
Manish652 <manishbhunia455@gmail.com>
2026-07-06 19:06:23 +0530

    Auth update

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,7 +2,6 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-// Allow only images and videos
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/") || file.mimetype.startsWith("video/")) {
     cb(null, true);
@@ -11,12 +10,10 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// Set 200MB upload limit (for videos and high-res images)
 const limits = {
   fileSize: 200 * 1024 * 1024, // 200 MB
 };
 
-// Create the upload middleware
 const upload = multer({
   storage,
   fileFilter,
--- utils/GenarateOtp.js
+++ utils/GenarateOtp.js
@@ -1,5 +1,4 @@
 const genarateOtp = () => {
     return Math.floor(100000 + Math.random() * 900000).toString();
 }
-
 export default genarateOtp;
